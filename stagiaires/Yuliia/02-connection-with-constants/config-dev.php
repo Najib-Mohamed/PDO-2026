ddc2cbbb367743b433019e265c5f9bc29eeb65e3
julia-dm <yulia.2508dmitruk@gmail.com>
2026-04-24 09:57:41 +0200

    Create config-dev.php for development

---
+++ stagiaires/Yuliia/02-connection-with-constants/config-dev.php
@@ -0,0 +1,11 @@
+<?php
+# config-dev.php
+# fichier de configuration de PDO en mode dev.
+# Ce fichier peut être sur github car il ne contient pas de données sensibles
+const DB_CONNECT_TYPE = "mysql"; // MySQL et MariaDB
+const DB_CONNECT_HOST = "localhost";//hôte local WAMP
+const DB_CONNECT_PORT = 3306;//port vers MariaDB
+const DB_CONNECT_NAME = "pdo_c2";//nom de la DB
+const DB_CONNECT_CHARSET = "utf8";//encodage de notre connexion
+const DB_CONNECT_USER = "root";// login par défaut
+const DB_CONNECT_PWD = "";//pas de mot de passe par défaut
