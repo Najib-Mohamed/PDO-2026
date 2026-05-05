7a94fa2ba6af398ada4a8d58de625773ba574be6
WebDevCF2m2026 <gitweb@cf2m.be>
2026-05-05 15:37:19 +0200

    04 prepare1

--- Exercices/04-prepare/public/index.php
+++ Exercices/04-prepare/public/index.php
@@ -3,12 +3,12 @@
 
 # On charge les dépendances, on va prendre celui
 # de développement (qui va sur github car local)
-require_once 'config-dev.php';
+require_once '../config-dev.php';
 
 // tentative de connection
 try {
     $db = new PDO(
-        dsn: MARIA_DNS,
+        dsn: MARIA_DSN,
         username: DB_CONNECT_USER,
         password: DB_CONNECT_PWD,
     );
@@ -69,61 +69,3 @@ if ($nbArticle === 0) {
 };
 
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Livre d'or</title>
-    <link rel="stylesheet" href="style.css">
-    
-</head>
-
-<body>
-    <?php if (isset($reussite)) {
-        echo $reussite;
-    } ?>
-    <div class="container">
-        <h1>📖 Livre d'or</h1>
-        <?php if (isset($erreur)) {
-            echo ($erreur);
-        }; ?>
-        <div class="form-box">
-            <form method="POST" action="">
-
-                <div class="form-group">
-                    <label for="email">Votre Email :</label>
-                    <input type="email" id="email" name="email" required>
-                </div>
-                <div class="form-group">
-                    <label for="title">Votre title :</label>
-                    <input type="text" id="title" name="title" required>
-                </div>
-
-                <div class="form-group">
-                    <label for="text">Votre Commentaire :</label>
-                    <textarea id="text" name="text" required></textarea>
-                </div>
-
-                <button type="submit" class="btn">Laisser un Commentaire</button>
-            </form>
-        </div>
-
-        <?php if (isset($message)) { ?>
-            <div class="message"><?php echo $message; ?></div>
-            <div class="comments">Nombre de commentaires : <?php echo $nbArticle; ?></div>
-        <?php } ?>
-    </div>
-    <div class="comments-list">
-        <?php foreach($articles as $article): ?>
-            <div class="commentaires-utilisateur">
-                <h3><?= htmlspecialchars($article["title"], ENT_QUOTES, 'UTF-8') ?></h3>
-                <p><?= htmlspecialchars($article["datetime"], ENT_QUOTES, 'UTF-8') ?></p>
-                <p><?= htmlspecialchars($article["texte"], ENT_QUOTES, 'UTF-8') ?></p>
-            </div>
-        <?php endforeach; ?>
-    </div>
-</body>
-
-</html>
